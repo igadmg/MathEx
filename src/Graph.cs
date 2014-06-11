654622eda24b09706ea259a12301818c4d6e17be
Igor Karatayev <igor.cwer@gmail.com>
2014-06-12 00:32:03 +0300

    Graph

---
+++ src/Graph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathEx
+{
+	[Serializable]
+	public class Graph<T, U> : Set<T>
+	{
+		public class Edge : Tuple<long, long, float>
+		{
+			public long a { get { return Item1; } }
+			public long b { get { return Item2; } }
+
+			U o_;
+
+
+			public Edge(long a, long b)
+				: base(a, b, float.NaN)
+			{ }
+
+			public Edge(long a, long b, float f)
+				: base(a, b, f)
+			{ }
+
+			public Edge(long a, long b, float f, U o)
+				: base(a, b, f)
+			{ o_ = o; }
+
+			public override bool Equals(object obj)
+			{
+				var e = obj as Edge;
+				return e != null ? (e.a == a) && (e.b == b) : base.Equals(obj);
+			}
+
+			public override int GetHashCode()
+			{
+				return a.GetHashCode() ^ b.GetHashCode();
+			}
+		}
+
+		protected HashSet<Edge> edges_ = new HashSet<Edge>();
+
+
+		public override int Remove(T item)
+		{
+			var ri = base.Remove(item);
+
+			var nedges = new HashSet<Edge>();
+			foreach (var edge in edges_) {
+				if (edge.a == ri || edge.b == ri)
+					continue;
+				if (edge.a > ri) edge.Item1--;
+				if (edge.b > ri) edge.Item2--;
+
+				nedges.Add(edge);
+			}
+			edges_ = nedges;
+
+			return ri;
+		}
+
+
+		public virtual bool Link(long a, long b, float d, U o)
+		{
+			if (edges_.Contains(new Edge(a, b)) || edges_.Contains(new Edge(b, a)))
+				return false;
+
+			edges_.Add(new Edge(a, b, d, o));
+			edges_.Add(new Edge(b, a, d, o));
+
+			return true;
+		}
+	}
+}
---
+++ src/Set.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathEx
+{
+	[Serializable]
+	public class Set<T>
+	{
+		protected List<T> nodes_ = new List<T>();
+
+		public virtual void Add(T item)
+		{
+			nodes_.Add(item);
+		}
+
+		public virtual int Remove(T item)
+		{
+			var i = nodes_.IndexOf(item);
+			if (i < 0) return i;
+
+			nodes_.RemoveAt(i);
+			return i;
+		}
+	}
+}
