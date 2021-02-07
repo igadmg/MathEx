dd4e2600f1534dc8a374f52bb3367b435b535f86
Igor Karatayev <igor.cwer@gmail.com>
2021-02-07 22:02:55 +0300

    Optimized TransformStack?

--- src/TransformStack.cs
+++ src/TransformStack.cs
@@ -1,35 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using SystemEx;
 
 namespace MathEx
 {
 	public class TransformStack
 	{
-		Stack<matrix4x4> transformStack = new Stack<matrix4x4>();
+		TransformStack parent;
 		public matrix4x4 currentTransform = matrix4x4.identity;
 
 
 		public vec2 zero2 => (vec2.zero.xyzw(0, 1) * currentTransform).xy();
 
 
-		public void Push(matrix4x4 transform)
-		{
-			transformStack.Push(currentTransform);
-			currentTransform = transform * currentTransform;
-		}
+		public TransformStack Push(matrix4x4 transform)
+			=> new TransformStack {
+				parent = this,
+				currentTransform = transform * currentTransform
+			};
 
-		public void Pop()
-		{
-			currentTransform = transformStack.Pop();
-		}
-
-		public IDisposable BeginTransform(vec2 translate)
-		{
-			Push(matrix4x4.Translate(translate.xyz(0)));
-			return DisposableLock.Lock(() => Pop());
-		}
+		public TransformStack Pop()
+			=> parent;
 
 
 		public static vec2 operator %(vec2 a, TransformStack b)
@@ -43,5 +33,16 @@ namespace MathEx
 
 		public static vec2 operator *(vec2i a, TransformStack b)
 			=> (a.xyzw(0, 1) * b.currentTransform).xy();
+	}
+
+	public class TrasformStackCursor
+	{
+		public TransformStack ts { get; protected set; } = new TransformStack();
+
+		public IDisposable BeginTransform(vec2 translate)
+		{
+			ts = ts.Push(matrix4x4.Translate(translate.xyz(0)));
+			return DisposableLock.Lock(() => ts = ts.Pop());
+		}
 	}
 }
