138c8955271e982695335196d8021a706c5ec267
Igor Karatayev <igor.cwer@gmail.com>
2021-02-07 22:30:42 +0300

    That is one strange form of inverted transform in TransformStack

--- src/TransformStack.cs
+++ src/TransformStack.cs
@@ -7,10 +7,16 @@ namespace MathEx
 	{
 		TransformStack parent;
 		public matrix4x4 currentTransform = matrix4x4.identity;
+		Lazy<matrix4x4> inverseTransform_;
+		public matrix4x4 inverseTransform => inverseTransform_.Value;
 
 
 		public vec2 zero2 => (vec2.zero.xyzw(0, 1) * currentTransform).xy();
 
+		public TransformStack()
+		{
+			inverseTransform_ = new Lazy<matrix4x4>(() => currentTransform.inverted);
+		}
 
 		public TransformStack Push(matrix4x4 transform)
 			=> new TransformStack {
@@ -33,6 +39,19 @@ namespace MathEx
 
 		public static vec2 operator *(vec2i a, TransformStack b)
 			=> (a.xyzw(0, 1) * b.currentTransform).xy();
+
+
+		public static vec2 operator %(TransformStack a, vec2 b)
+			=> (b.xyzw(0, 0) * a.inverseTransform).xy();
+
+		public static vec2 operator %(TransformStack a, vec2i b)
+			=> (b.xyzw(0, 0) * a.inverseTransform).xy();
+
+		public static vec2 operator *(TransformStack a, vec2 b)
+			=> (b.xyzw(0, 1) * a.inverseTransform).xy();
+
+		public static vec2 operator *(TransformStack a, vec2i b)
+			=> (b.xyzw(0, 1) * a.inverseTransform).xy();
 	}
 
 	public class TrasformStackCursor
